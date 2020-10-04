ddedaa41de08bc102de9feb1b4b29d709894e767
Rachel Kat <rachelkat@gmail.com>
2020-10-04 16:32:14 -0400

    finished section 3

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+  // public name: string;
+  // public amount: number;
+
+  // line 6 is an alternate approach to the implementations in comments
+  // constructor(name: string, amount: number) {
+  constructor(public name: string, public amount: number) {
+    // this.name = name;
+    // this.amount = amount;
+  }
+}
